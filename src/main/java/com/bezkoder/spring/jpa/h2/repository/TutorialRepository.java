d7a495da228141467edadbbcddc644475a314a7e
gsaini092-dev <50400912+gsaini092-dev@users.noreply.github.com>
2022-12-17 21:39:09 +0530

    Initial commit

---
+++ src/main/java/com/bezkoder/spring/jpa/h2/repository/TutorialRepository.java
@@ -0,0 +1,17 @@
+package com.bezkoder.spring.jpa.h2.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bezkoder.spring.jpa.h2.model.Tutorial;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+  List<Tutorial> findByPublished(boolean published);
+
+  List<Tutorial> findByTitleContaining(String title);
+
+  //List<Tutorial> findByDescriptionContaining(String description);
+}
